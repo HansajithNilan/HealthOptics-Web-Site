13caea6ddc869779864d365a0d9fcd0906b5789c
NilanHansajith <nilanhansajith7@gmail.com>
2025-03-08 12:36:43 +0530

    Home Page Develop

---
+++ Frontend/src/components/Footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './footer.css'
+function footer() {
+  return (
+    <div className='footer'>
+    <p> 2025 HealthOptics.All rights reserved.</p>
+    <ul>
+      <li>Terms of Services</li>
+      <li>Privacy policy</li>
+    </ul>
+  </div>
+  )
+}
+
+export default footer
