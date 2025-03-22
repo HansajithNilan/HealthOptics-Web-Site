c04065121d002bba48fb8e04a4542833b300bc3a
shakya <152844822+kyashark@users.noreply.github.com>
2025-03-23 02:14:23 +0530

    Update Admin Dashbaord

---
+++ Frontend/src/pages/AdminPage/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "../../../components/DashboardLayout/DashboardLayout.jsx";
+
+const Dashboard = () => {
+  return (
+    <DashboardLayout title="Dashboard">
+      <h1>Hi</h1>
+    </DashboardLayout>
+  );
+};
+
+export default Dashboard;
+
+
