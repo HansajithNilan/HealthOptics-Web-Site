15e129729cd9cfed85c661a6f1c6aa5f945abad8
shakya <152844822+kyashark@users.noreply.github.com>
2025-03-22 12:38:20 +0530

    Update Sidebar

--- Frontend/src/components/SideBar/SideBar.jsx
+++ Frontend/src/components/SideBar/SideBar.jsx
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./SideBar.css";
 
-function SideBar(){
-    return(
-        <div className="navbar">
-        <div className="side-bar-logo">
-          <h1>Admin Panel</h1>
-        </div>
-        <ul className="side-bar-links">
-          <li>
-            <Link to="/admin/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/admin/spectacles">Manage Spectacles</Link>
-          </li>
-          <li>
-            <Link to="/admin/orders">Manage Orders</Link>
-          </li>
-          <li>
-            <Link to="/admin/customers">Manage Customers</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link> {/* You can replace it with actual logout functionality later */}
-          </li>
-        </ul>
+function SideBar() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`sidebar ${isOpen ? "open" : "closed"}`}>
+      <div className="sidebar-header">
+        <img src="../../../public/Website_logo.png"/>
+        <h1 className="logo">Health Optics</h1>
+        {/* <button className="toggle-btn" onClick={toggleSidebar}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </button> */}
       </div>
-    );
+      <ul className="sidebar-links">
+        <li>
+          <Link to="/admin/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/admin/spectacles">Spectacles Managment</Link>
+        </li>
+        <li>
+          <Link to="/admin/doctor">Doctor Managment</Link>
+        </li>
+        <li>
+          <Link to="/admin/reservations">Reservations Managment</Link>
+        </li>
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SideBar;
