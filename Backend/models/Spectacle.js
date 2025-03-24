1b461b36b5a231812231144b968515d73252857f
shakya <152844822+kyashark@users.noreply.github.com>
2025-03-24 12:11:06 +0530

    Update Spectacle model

--- Backend/models/Spectacle.js
+++ Backend/models/Spectacle.js
@@ -56,6 +56,10 @@ const spectacleSchema = new Schema({
         type: Number,
         required: true
     },
+    stock:{
+        type: Number,
+        required: true
+    },
     rating : {
         type: Number,
         required: true
